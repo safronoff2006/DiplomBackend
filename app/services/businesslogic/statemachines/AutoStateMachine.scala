39e90076688f607ae5678a31adaddd69f35ca708
Валерий Сафронов <safronoff2006@gmail.com>
2023-08-02 22:11:15 +0300

    Commit 15

--- app/services/businesslogic/statemachines/AutoStateMachine.scala
+++ app/services/businesslogic/statemachines/AutoStateMachine.scala
@@ -2,15 +2,29 @@ package services.businesslogic.statemachines
 
 import executioncontexts.CustomBlockingExecutionContext
 import models.extractors.NoCardOrWithCard
+import models.extractors.Protocol2NoCard.NoCard
+import models.extractors.Protocol2WithCard.WithCard
 import play.api.Logger
 
 import javax.inject.Inject
+
+object AutoStateMachine {
+  case class StateAutoPlatform()
+}
 
 class AutoStateMachine  @Inject() (implicit ex: CustomBlockingExecutionContext) extends StateMachine() {
   val logger: Logger = Logger(this.getClass)
   logger.info("Создана стейт машина AutoStateMachine")
 
-  override def protocolExecute(message: NoCardOrWithCard): Unit = {
 
+
+
+  override def protocolExecute(message: NoCardOrWithCard): Unit = {
+    message match {
+      case protocolObject: NoCard => println(s"NO CARD: $protocolObject")
+      case protocolObject: WithCard => println(s"WITH CARD: $protocolObject")
+      case _ =>
+    }
   }
+
 }
--- app/services/businesslogic/statemachines/StateMachine.scala
+++ app/services/businesslogic/statemachines/StateMachine.scala
@@ -2,8 +2,6 @@ package services.businesslogic.statemachines
 
 import executioncontexts.CustomBlockingExecutionContext
 import models.extractors.NoCardOrWithCard
-import models.extractors.Protocol2NoCard.NoCard
-import models.extractors.Protocol2WithCard.WithCard
 
 import java.util.concurrent.{BlockingQueue, LinkedBlockingQueue}
 import javax.inject.Inject
@@ -29,10 +27,7 @@ abstract class StateMachine @Inject() (implicit ex: CustomBlockingExecutionConte
   ex.execute(() => {
     while (!Thread.interrupted()) {
       val message = queue.take()
-      message match {
-        case obj: NoCard => println(s"NO CARD: $obj на стейт машине $name")
-        case obj: WithCard => println(s"WITH CARD: $obj  на стейт машине $name")
-      }
+     protocolExecute(message)
     }
   })
 
