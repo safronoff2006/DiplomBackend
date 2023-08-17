370ab4a5d0411e79e64a7f5e9ab319516bf97f05
Валерий Сафронов <safronoff2006@gmail.com>
2023-08-17 15:41:17 +0300

    Commit 30

--- app/test/command_scripts/setdata24000in.sh
+++ app/test/command_scripts/setdata24000in.sh
@@ -1,1 +1,1 @@
-echo '{"command":"setdata", "id":"Второй", "data": "v-+++ 24000%0000."}' > /dev/udp/127.0.0.1/9010
+echo '{"command":"setdata", "id":"Второй", "data": "v-+++ 24000R%0000."}' > /dev/udp/127.0.0.1/9010
--- app/test/command_scripts/setdata4000.sh
+++ app/test/command_scripts/setdata4000.sh
@@ -1,5 +1,5 @@
-echo '{"command":"setdata", "id":"Второй", "data": "v++++  4000%0000."}' > /dev/udp/127.0.0.1/9010
-echo '{"command":"setdata", "id":"Третий", "data": "v++++  4000%0000."}' > /dev/udp/127.0.0.1/9010
+echo '{"command":"setdata", "id":"Второй", "data": "v++++  4000R%0000."}' > /dev/udp/127.0.0.1/9010
+echo '{"command":"setdata", "id":"Третий", "data": "v++++  4000G%0000."}' > /dev/udp/127.0.0.1/9010
 
 
 
